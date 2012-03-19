6be32bee22b950cf6245605a86d0b97fac8e291b
grave <grave.coder@gmail.com>
2012-03-19 19:42:17 +0800

    some bpmn test

--- drools-verify/src/test/java/verify/jboss/jbpm/quickstarts/JavaServiceQuickstartTest.java
+++ drools-verify/src/test/java/verify/jboss/jbpm/quickstarts/JavaServiceQuickstartTest.java
@@ -21,13 +21,13 @@ public class JavaServiceQuickstartTest extends JbpmUnitTestCase {
 		ksession.startProcess("com.sample.script", params);
 	}
 	
-	@Test
+	/*@Test
 	public void testConditionSimpleScript() {
 		StatefulKnowledgeSession ksession = createKnowledgeSession("quickstarts/ConditionScriptTask.bpmn");
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("person", new Person("john"));
 		ksession.startProcess("com.sample.script", params);
-	}
+	}*/
 
 
 }
